d4028224bd5c189ad02af7cb2211dd1a6069d9b9
Uthpala Jayawardena <Uthpala Jayawardena@Uthpala>
2018-07-17 19:19:59 +0530

    New testing

---
+++ test/Class/Station/sensorStationTest.java
@@ -0,0 +1,217 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class.Station;
+
+import java.util.Observable;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Uthpala Jayawardena
+ */
+public class sensorStationTest {
+    
+    public sensorStationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSensorStation_ID method, of class sensorStation.
+     */
+    @Test
+    public void testGetSensorStation_ID() {
+        System.out.println("getSensorStation_ID");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getSensorStation_ID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSensorStation_ID method, of class sensorStation.
+     */
+    @Test
+    public void testSetSensorStation_ID() {
+        System.out.println("setSensorStation_ID");
+        String sensorStation_ID = "";
+        sensorStation instance = null;
+        instance.setSensorStation_ID(sensorStation_ID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStation_Type method, of class sensorStation.
+     */
+    @Test
+    public void testGetStation_Type() {
+        System.out.println("getStation_Type");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getStation_Type();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStation_Type method, of class sensorStation.
+     */
+    @Test
+    public void testSetStation_Type() {
+        System.out.println("setStation_Type");
+        String Station_Type = "";
+        sensorStation instance = null;
+        instance.setStation_Type(Station_Type);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStation_Name method, of class sensorStation.
+     */
+    @Test
+    public void testGetStation_Name() {
+        System.out.println("getStation_Name");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getStation_Name();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStation_Name method, of class sensorStation.
+     */
+    @Test
+    public void testSetStation_Name() {
+        System.out.println("setStation_Name");
+        String Station_Name = "";
+        sensorStation instance = null;
+        instance.setStation_Name(Station_Name);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLocation method, of class sensorStation.
+     */
+    @Test
+    public void testGetLocation() {
+        System.out.println("getLocation");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getLocation();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLocation method, of class sensorStation.
+     */
+    @Test
+    public void testSetLocation() {
+        System.out.println("setLocation");
+        String Location = "";
+        sensorStation instance = null;
+        instance.setLocation(Location);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLongitude method, of class sensorStation.
+     */
+    @Test
+    public void testGetLongitude() {
+        System.out.println("getLongitude");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getLongitude();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLongitude method, of class sensorStation.
+     */
+    @Test
+    public void testSetLongitude() {
+        System.out.println("setLongitude");
+        String Longitude = "";
+        sensorStation instance = null;
+        instance.setLongitude(Longitude);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLatitude method, of class sensorStation.
+     */
+    @Test
+    public void testGetLatitude() {
+        System.out.println("getLatitude");
+        sensorStation instance = null;
+        String expResult = "";
+        String result = instance.getLatitude();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLatitude method, of class sensorStation.
+     */
+    @Test
+    public void testSetLatitude() {
+        System.out.println("setLatitude");
+        String Latitude = "";
+        sensorStation instance = null;
+        instance.setLatitude(Latitude);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of update method, of class sensorStation.
+     */
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        Observable o = null;
+        Object o1 = null;
+        sensorStation instance = null;
+        instance.update(o, o1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
